a2a85979d26eeac68c2f8d0e9a42582f763ca6f0
Yura Home <www.but93@gmail.com>
2024-03-10 21:20:23 +0200

    slider

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper(".swiper", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+        // renderBullet: function (index, className) {
+        //   return '<span class="' + className + '">' + (index + 1) + "</span>";
+        // },
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
